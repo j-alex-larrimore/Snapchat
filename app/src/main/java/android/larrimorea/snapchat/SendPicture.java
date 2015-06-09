0285a0eb2eb1e85dd37e1b0e59e68c5ef7511e50
Alex Larrimore <j.alex.larrimore@gmail.com>
2015-06-09 14:58:57 -0700

    Updated manifest to allow bluetooth, added basic onCreate and onActivityResult functions to SendPicture.java

--- app/src/main/java/android/larrimorea/snapchat/SendPicture.java
+++ app/src/main/java/android/larrimorea/snapchat/SendPicture.java
@@ -1,9 +1,46 @@
 package android.larrimorea.snapchat;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Alex on 6/9/2015.
  */
 public class SendPicture extends Activity {
+    private int REQUEST_ENABLE_BT = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_image);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            Log.i("SendPicture", "Bluetooth Not Enabled");
+        }
+
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Bluetooth Connection!", Toast.LENGTH_LONG).show();
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Bluetooth Canceled!", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Bluetooth Failed!", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
