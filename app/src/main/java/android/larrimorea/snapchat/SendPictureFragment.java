bd3e3deac92723bb81cd49309efe0aac7d7e89de
Alex Larrimore <j.alex.larrimore@gmail.com>
2015-07-01 11:10:55 -0700

    This should have been 2 commits. Before coding, needed to delete Friends and FriendRequests arrays from Parse and add them back in as relations. Then, built in functionality so whenever you clicked on Add a Friend button it would add a generic name to your friendrequests relation in Parse. Finally, added a popup dialog that allows users to enter a name to search for and actually search parse. If found that user is added to original user's friendrequests relation.

--- app/src/main/java/android/larrimorea/snapchat/SendPictureFragment.java
+++ app/src/main/java/android/larrimorea/snapchat/SendPictureFragment.java
@@ -1,6 +1,7 @@
 package android.larrimorea.snapchat;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
@@ -9,6 +10,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,9 +22,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +43,7 @@ public class SendPictureFragment extends Fragment {
     private ArrayAdapter<String> mArrayAdapter;
     protected ListView listView;
     private static Uri selectedPic = null;
+    private String mFriendReqName;
 
     @Nullable
     @Override
@@ -45,7 +60,7 @@ public class SendPictureFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity(), ChoosePicFragment.class);
-                String str = (String)mArrayAdapter.getItem(position);
+                String str = (String) mArrayAdapter.getItem(position);
                 Toast.makeText(getActivity(), "Clicked -" + str, Toast.LENGTH_LONG).show();
                 startActivity(intent);
             }
@@ -79,9 +94,83 @@ public class SendPictureFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.menu_item_add_friend:
+                makePopup();
+
+
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void makePopup(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+        alert.setTitle("Type your Friend's Name");
+
+// Set an EditText view to get user input
+        final EditText input = new EditText(getActivity());
+        alert.setView(input);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mFriendReqName = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if(mFriendReqName!= null){
+                    searchForFriend();
+                    dialog.cancel();
+                }
+                // Do something with value!
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        alert.show();
+    }
+
+    public void searchForFriend(){
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("username", mFriendReqName);
+        Log.i("SendPicFrag", "Options" + mFriendReqName);
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, com.parse.ParseException e) {
+
+                if (e == null) {
+                    Log.i("SendPictureFragment", "Found!");
+                    sendFriendRequest(user);
+                } else {
+                    Toast.makeText(getActivity(), "Friend not found!", Toast.LENGTH_SHORT).show();
+                    Log.e("SendPictureFragment", "AddFriend " + e);
+                }
+            }
+        });
+    }
+
+    public void sendFriendRequest(ParseUser user){
+            ParseRelation<ParseUser> relation = ParseUser.getCurrentUser().getRelation("friendrequests");
+            relation.add(user);
+            ParseUser.getCurrentUser().saveInBackground();
+            Log.i("SendPicture", "Sending Friend Request to " + user.getUsername());
     }
 }
