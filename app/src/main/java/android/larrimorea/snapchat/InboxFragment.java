7dbadb2312ee4fa0dec8444babd84c59bca024a4
Alex Larrimore <j.alex.larrimore@gmail.com>
2015-07-02 10:40:41 -0700

    Everything fully functional, still need to do some bug hunting

--- app/src/main/java/android/larrimorea/snapchat/InboxFragment.java
+++ app/src/main/java/android/larrimorea/snapchat/InboxFragment.java
@@ -16,11 +16,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
@@ -36,8 +40,11 @@ import java.util.List;
 public class InboxFragment extends Fragment {
 
     private ArrayList<String> frArrayStrings = new ArrayList<String>();
+    private ArrayList<String> picArrayStrings = new ArrayList<String>();
     private ArrayAdapter mAdapter;
+    private ArrayAdapter mPicAdapter;
     private ListView requestView;
+    private ListView picView;
     private View mView;
     private ParseUser mFriend;
 
@@ -47,6 +54,7 @@ public class InboxFragment extends Fragment {
         mView = inflater.inflate(R.layout.inbox, container, false);
 
         getFriendRequests();
+        getSentPics();
 
         return mView;
     }
@@ -63,7 +71,7 @@ public class InboxFragment extends Fragment {
                     displayFriendRequests();
                 } else {
                     Toast.makeText(getActivity(), "No Friend Requests Found", Toast.LENGTH_SHORT);
-                    Log.e("score", "Error: " + e.getMessage());
+                    Log.e("Inbox", "getFriendReqError: " + e.getMessage());
                 }
             }
         });
@@ -82,7 +90,47 @@ public class InboxFragment extends Fragment {
         requestView = (ListView) mView.findViewById(R.id.listViewFriendReqs);
         mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, frArrayStrings);
         requestView.setAdapter(mAdapter);
-        setClickListener();
+
+    }
+
+
+    private void acceptRequest(final String from){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FriendRequests");
+        query.whereEqualTo("To", ParseUser.getCurrentUser().getUsername());
+        query.whereEqualTo("From", from);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, com.parse.ParseException e) {
+                if (e == null) {
+                    parseObject.put("Accepted", true);
+                    parseObject.saveInBackground();
+                    ParseRelation<ParseUser> relation = ParseUser.getCurrentUser().getRelation("friends");
+                    addFriend(from, relation);
+                } else {
+                    Log.e("Inbox", "acceptRequestError: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void addFriend(final String from, final ParseRelation relation){
+        Log.i("Inbox", "Adding Friend");
+        mFriend = null;
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("username", from);
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser parseUser, com.parse.ParseException e) {
+                if (e == null) {
+                    mFriend = parseUser;
+                    relation.add(mFriend);
+                    ParseUser.getCurrentUser().saveInBackground();
+                } else {
+                    Log.e("inbox", "getFriendError: " + e.getMessage());
+
+                }
+            }
+        });
     }
 
     private void setClickListener(){
@@ -91,11 +139,53 @@ public class InboxFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 makePopup(requestView.getItemAtPosition(position).toString());
+
+            }
+        });
+        picView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                acceptPic(picView.getItemAtPosition(position).toString());
+
             }
         });
     }
 
-    public void makePopup(final String from){
+    private void getSentPics(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("SentPicture");
+        query.whereEqualTo("To", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, com.parse.ParseException e) {
+                if (e == null) {
+                    fillSentPics(list);
+                    displayPics();
+                } else {
+                    Toast.makeText(getActivity(), "No Pictures Found", Toast.LENGTH_SHORT);
+                    Log.e("Inbox", "getPicsError: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void fillSentPics(List<ParseObject> list){
+        for(ParseObject ob: list){
+            picArrayStrings.add(ob.get("From").toString());
+        }
+        if(picArrayStrings == null){
+            picArrayStrings.add("No Pending Friend Requests");
+        }
+    }
+
+    private void displayPics(){
+        picView = (ListView) mView.findViewById(R.id.listViewInbox);
+        mPicAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, picArrayStrings);
+        picView.setAdapter(mPicAdapter);
+        setClickListener();
+    }
+
+    private void makePopup(final String from){
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
         alert.setTitle("Accept Friend Request?");
@@ -119,42 +209,53 @@ public class InboxFragment extends Fragment {
         alert.show();
     }
 
-    public void acceptRequest(final String from){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("FriendRequests");
+    private void acceptPic(String from){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("SentPicture");
         query.whereEqualTo("To", ParseUser.getCurrentUser().getUsername());
         query.whereEqualTo("From", from);
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject parseObject, com.parse.ParseException e) {
                 if (e == null) {
-                    parseObject.put("Accepted", true);
-                    parseObject.saveInBackground();
-                    ParseRelation<ParseUser> relation = ParseUser.getCurrentUser().getRelation("friends");
-                    addFriend(from, relation);
+                    viewPic(parseObject.getParseFile("Picture"));
+                    try {
+                        parseObject.delete();
+                    } catch (com.parse.ParseException e1) {
+                        e1.printStackTrace();
+                    }
                 } else {
-                    Log.e("Inbox", "acceptRequestError: " + e.getMessage());
+                    Log.e("Inbox", "InboxError: " + e.getMessage());
                 }
             }
         });
     }
 
-    public void addFriend(final String from, final ParseRelation relation){
-        Log.i("Inbox", "Adding Friend");
-        mFriend = null;
-        ParseQuery<ParseUser> query = ParseUser.getQuery();
-        query.whereEqualTo("username", from);
-        query.getFirstInBackground(new GetCallback<ParseUser>() {
-            @Override
-            public void done(ParseUser parseUser, com.parse.ParseException e) {
-                if (e == null) {
-                    mFriend = parseUser;
-                    relation.add(mFriend);
-                    ParseUser.getCurrentUser().saveInBackground();
-                } else {
-                    Log.e("inbox", "getFriendError: " + e.getMessage());
+    private void viewPic(ParseFile pic){
+        if(pic != null){
+            makePicPopup(pic);
+        }
+    }
 
-                }
+    private void makePicPopup(ParseFile pic){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+        final ParseImageView image = new ParseImageView(getActivity());
+        alert.setView(image);
+
+        image.setParseFile(pic);
+        image.loadInBackground(new GetDataCallback() {
+            @Override
+            public void done(byte[] bytes, com.parse.ParseException e) {
+
             }
         });
+
+        alert.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        alert.show();
     }
 }
